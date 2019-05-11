9519a8bfd4eb129da82c96eb625164322df8eb76
Rohit Yadav <rohit@turbot.com>
2019-05-11 12:30:13 +0530

    trying to find branch

--- myecho.sh
+++ myecho.sh
@@ -27,34 +27,35 @@ function pushed_files {
 	# branch_name='add-aws-ec2-cmdb'
   commit_id=$CODEBUILD_RESOLVED_SOURCE_VERSION
   git branch
-  branch_name=`git branch --contains $commit_id`
+  branch_name=`git branch --contains $commit_id` > log.txt
+  cat log.txt
   echo $branch_name
   echo $commit_id
 	git checkout $branch_name
   echo "subham suggestions"
   git branch
-	if [[ `git diff --name-only master $branch_name` ]];
-  then
-   	echo "Files got changed"
-    FILES=`git diff --name-only master $branch_name`
-  	echo $FILES
-  	for val in $FILES;
-    do
-  	  path=$(cut -d'/' -f 1-2 <<<$val)
-      echo $path
-  		cd $path
-      pwd
-      echo backchodi chal rahi hai bhai
-  		# turbot inspect
-  		# if [ "$?" -ne "0" ]
-  		# then
-    	# 	error_msg "inspect not running"
-    	# 	error+="$path service package has error"
-  		# fi
-      # cd ../../
-    done
-  fi
-  git checkout master
+	# if [[ `git diff --name-only master $branch_name` ]];
+  # then
+  #  	echo "Files got changed"
+  #   FILES=`git diff --name-only master $branch_name`
+  # 	echo $FILES
+  # 	for val in $FILES;
+  #   do
+  # 	  path=$(cut -d'/' -f 1-2 <<<$val)
+  #     echo $path
+  # 		cd $path
+  #     pwd
+  #     echo backchodi chal rahi hai bhai
+  # 		# turbot inspect
+  # 		# if [ "$?" -ne "0" ]
+  # 		# then
+  #   	# 	error_msg "inspect not running"
+  #   	# 	error+="$path service package has error"
+  # 		# fi
+  #     # cd ../../
+  #   done
+  # fi
+  # git checkout master
 }
 
 function error_msg {
